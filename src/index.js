474456a345af0f83a59f0ee07627bc1d07bb007c
Sigurd Drivstuen <sigurd.drivstuen@trondheim.kommune.no>
2026-01-26 09:02:30 +0100

    Add delete drafts releases and refac getting variables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import { getInput } from "@actions/core";
 import { getOctokit, context } from "@actions/github";
 
 async function deleteTag() {
   try {
-    const octokit = getOctokit(process.env.GITHUB_TOKEN);
+    const token = getInput("github_token", { required: true });
+    const octokit = getOctokit(token);
 
     const { owner, repo } = context.repo;
 
@@ -11,29 +13,27 @@ async function deleteTag() {
       repo,
     });
 
-    if (!tags || !Array.isArray(tags)) {
-      console.log("No tags found in the repository.");
-      return;
-    }
-
-    if (tags.length === 0) {
+    if (!tags || !Array.isArray(tags) || tags.length === 0) {
       console.log("No tags found in the repository.");
       return;
     }
 
     console.log(`Found ${tags.length} tags in the repository.`);
 
+    const deletionThresholdDays = parseInt(
+      getInput("deletionThreshold") || "30",
+      10,
+    );
+
     const tagsToBeDeleted = [];
     const now = Date.now();
-    const THIRTY_DAYS_MS = 30 * 24 * 60 * 60 * 1000;
+    const THIRTY_DAYS_MS = deletionThresholdDays * 24 * 60 * 60 * 1000;
 
     for (const tag of tags) {
       const split = tag.name.split("+");
 
       if (!split[1]) {
-        if (split[0].includes("-").length === 1) {
-          continue;
-        }
+        if (split[0].includes("-").length === 1) continue;
       }
 
       if (split[1].split("-").length === 1) continue;
@@ -72,6 +72,45 @@ async function deleteTag() {
     }
   } catch (error) {
     console.error("Error deleting tags:", error);
+    process.exit(1);
+  }
+
+  try {
+    console.log("---------------------------------");
+
+    // Default to true unless explicitly set to "false"
+    const shouldDeleteDraftReleases =
+      getInput("deleteDraftReleases") !== "false";
+
+    if (!shouldDeleteDraftReleases) {
+      console.log("Skipping deletion of draft releases.");
+      return;
+    }
+
+    console.log("Deleteing related draft releases");
+
+    const octokit = getOctokit(process.env.GITHUB_TOKEN);
+
+    const { owner, repo } = context.repo;
+
+    const releases = await octokit.paginate(octokit.rest.repos.listReleases, {
+      owner,
+      repo,
+    });
+
+    const draftReleases = releases.filter(({ draft }) => draft);
+
+    for (const release of draftReleases) {
+      cobsole.log(`Deleting draft release: ${release.name}`);
+
+      await octokit.rest.repos.deleteRelease({
+        owner,
+        repo,
+        release_id: release.id,
+      });
+    }
+  } catch (error) {
+    console.error("Error deleting draft releases:", error);
     process.exit(1);
   }
 }
